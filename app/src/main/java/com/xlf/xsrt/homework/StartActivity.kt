371995d18f3fdb410f4e3fd64e770fc0d21afe42
chenhong <932192914@qq.com>
2018-12-14 22:15:36 +0800

    项目搭建

---
+++ app/src/main/java/com/xlf/xsrt/homework/StartActivity.kt
@@ -0,0 +1,20 @@
+package com.xlf.xsrt.homework
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.xlf.xsrt.work.MainActivity
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+
+    fun logonClick(view : View){
+        var  intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
